cbb05c1402a3823c186fe5f8711beec4d915a181
Chandan Gupta <138652946+chandannewt@users.noreply.github.com>
2024-01-03 21:03:39 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
 
-	@GetMapping("/")
+	@Mapping("/")
 	public String greet() {
 		return "Hello world";
 	}
